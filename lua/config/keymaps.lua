2d487025c8c333fbcfd57450eb8177c1c82b500d
C0rvax <arthurduvillaret@gmail.com>
2023-12-02 21:32:19 +0100

    3rd push

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<A-i>", ":FloatermToggle<CR>")
+vim.keymap.set("t", "<A-i>", "<C-\\><C-n>:FloatermToggle<CR>")
+vim.keymap.set("t", "<A-n>", "norminette<CR>")
+vim.keymap.set("t", "<A-g>", "lazygit<CR>")
+vim.keymap.set("t", "<C-l>", "ft_lock<CR>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+vim.keymap.set("n", "<Leader>er", vim.diagnostic.open_float)
+
+vim.keymap.set("n", "<Leader>x", ":bd<CR>")
+vim.keymap.set("n", "<Tab>", ":bnext<CR>")
+
 vim.keymap.set("n", "<Leader>w", ":w!<cr>")
 vim.keymap.set("n", "<Leader>q", ":q!<cr>")
 
@@ -14,4 +26,8 @@ map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
 map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
 map("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+map("n", "<space>", "/")
+map("n", "<C-space>", "?")
+map("n", "<leader>te", ":tabedit ")
 
