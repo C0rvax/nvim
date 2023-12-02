213353a384fa55a4ccb868dcdd6643f8b108bbff
C0rvax <arthurduvillaret@gmail.com>
2023-12-02 20:58:59 +0100

    2nd push

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,14 @@
 
 vim.keymap.set("n", "<Leader>w", ":w!<cr>")
 vim.keymap.set("n", "<Leader>q", ":q!<cr>")
+
+local map = vim.keymap.set
+
+-- tabs
+
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
