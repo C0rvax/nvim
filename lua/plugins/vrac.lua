e139183e15f7b4ece23b3d0fd39f0e53bf5ab49b
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2024-01-16 11:47:03 +0100

    ajout de clangd mason

--- lua/plugins/vrac.lua
+++ lua/plugins/vrac.lua
@@ -83,5 +83,38 @@ return {
 
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+    {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "clangd",
+      },
+    },
+  },
 }
 
