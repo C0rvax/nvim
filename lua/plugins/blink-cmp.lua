364cd1dd7b444696b4fa85d9fecde4835cfdddae
c0rvax <C0rvax@pm.me>
2024-12-24 14:26:41 +0100

    fix de enter sans fallback et nouveau snippet cout

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -17,7 +17,7 @@ return {
     end },
 ]]
     -- Valide l'élément sélectionné
-    ['<CR>'] = { 'accept' },
+    ['<CR>'] = { 'accept', "fallback" },
     ["<Tab>"] = { "select_next", "fallback" },
     ["<S-Tab>"] = { "select_prev", "fallback" },
     [','] = { "hide", "fallback" },
